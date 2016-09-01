f945784a9c3e380a2939d6a5476d71f93d81ee14
Alejandro Mesa <alejom99@gmail.com>
2016-09-01 10:52:00 -0700

    Logic to get the logentries token from the host labels using the rancher metadata api.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 JMX_HOST=$(curl http://rancher-metadata/2015-12-19/self/host/agent_ip)
+CASSANDRA_METRICS_LOGENTRIES_TOKEN=$(curl http://rancher-metadata/2015-12-19/self/host/labels/cassandra_metrics_logentries_token)
 
 sed -i 's/%%JMX_PORT%%/'"$JMX_PORT"'/g; s/%%JMX_HOST%%/'"$JMX_HOST"'/g; s/%%JMX_USERNAME%%/'"$JMX_USERNAME"'/g; s/%%JMX_PASSWORD%%/'"$JMX_PASSWORD"'/g' /config/queries.json
+sed -i 's/%%CASSANDRA_METRICS_LOGENTRIES_TOKEN%%/'"$CASSANDRA_METRICS_LOGENTRIES_TOKEN"'/g' /config/log4j_logentries.xml
